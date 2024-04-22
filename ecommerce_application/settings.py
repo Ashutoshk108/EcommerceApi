edb20b839fea917634a2163669dd0feb4a052f4b
Ashutosh Mishra <Ashutosh.mishra@bhavnacorp.com>
2024-04-22 18:38:33 +0530

    updated code

--- ecommerce_application/settings.py
+++ ecommerce_application/settings.py
@@ -20,19 +20,8 @@ SECRET_KEY = 'django-insecure-4!2ec$-^7e=xja=u-+6t!*g0k-3888s66%xn7i60o$*t@hqd_i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
-
-SWAGGER_SETTINGS = {
-    'SECURITY_DEFINITIONS': {
-        'api_key': {
-            'type': 'apiKey',
-            'in': 'header',
-            'name': 'Authorization'
-        }
-    },
-    'USE_SESSION_AUTH': False,
-}
 
 # Application definition
 
@@ -88,7 +77,7 @@ DATABASES = {
         'NAME': 'ashutosh2135$sample',
         'USER': 'ashutosh2135',
         'PASSWORD': 'Mysql123',
-        'HOST': 'ashutosh2135.mysql.pythonanywhere-services.com',
+        'HOST': 'ashutosh2135.mysql.pythonanywhere-services.com',  
     }
 }
 
@@ -129,7 +118,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = '/home/ashutosh108k/mysite/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
